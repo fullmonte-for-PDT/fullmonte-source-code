e2a2e612569218cadb39d4b0baacb4ba3b3d1ac2
yasmin-afsharnejad <y.afsharnezhad@gmail.com>
2017-12-20 16:12:25 -0500

    Added the sub-function to git

--- Run_ctest.sh
+++ Run_ctest.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
+#function
 
-SRC=`pwd`
-
-
-
-# Release - Clang - VTK 7.1.1 - Boost 1.58.0
-
-
-cd FullMonteSW/Build/ReleaseClang
+call_Tests () {
  
 # ctest -R Test_VTK # does not run
 ctest -R Test_AffineTransform
@@ -28,97 +22,36 @@ ctest -R Test_RNG
 # The following tests should succeed if they run and generate output for comparison, working on them
  ctest -R Test_tclShell
  ctest -R Test_FullMonte_MMC_Comparison
- ctest -R Test_FullMonte_Self
- ctest -R Test_FullMonte_Approx
+ #ctest -R Test_FullMonte_Self
+ # ctest -R Test_FullMonte_Approx
  ctest -R Test_FullMonte_Composite
+}
+
+SRC=`pwd`
+
+# Release - Clang - VTK 7.1.1 - Boost 1.58.0
+
+cd FullMonteSW/Build/ReleaseClang
+call_Tests
 
 if [ 1 -eq 0 ]; then
 # Debug - Clang - VTK 7.1.1 - Boost 1.58.0
 cd $SRC
-
 cd FullMonteSW/Build/DebugClang
-
-# ctest -R Test_VTK # does not run
-ctest -R Test_AffineTransform
-ctest -R Test_Units
-ctest -R Test_Layered_Basic
-ctest -R Test_Queries
-# ctest -R Test_TIMOSReaderWriter  #This test fails
-# ctest -R Test_MCML #This test fails
-ctest -R Test_COMSOLMeshReaderWriter 
-# ctest -R Test_TetraEmitterFactory # This test fails
-# ctest -R Test_MCMLKernel_Sample # This test fails
-# ctest -R Test_InternalKernel # This test fails
-ctest -R Test_Mouse
-# ctest -R Test_BlockRandomDistribution # This test fails
-ctest -R Test_RNG
-# ctest -R DVH # This test fails
-# ctest -R DSH # This test fails
-# The following tests should succeed if they run and generate output for comparison, working on them
- ctest -R Test_tclShell
- ctest -R Test_FullMonte_MMC_Comparison
- ctest -R Test_FullMonte_Self
- ctest -R Test_FullMonte_Approx
- ctest -R Test_FullMonte_Composite
+call_Tests
 
 
 # RelWithDebInfo - Clang - VTK 7.1.1 - Boost 1.58.0
 cd $SRC
-
 cd FullMonteSW/Build/RelWithDebInfoClang
-
-# ctest -R Test_VTK # does not run
-ctest -R Test_AffineTransform
-ctest -R Test_Units
-ctest -R Test_Layered_Basic
-ctest -R Test_Queries
-# ctest -R Test_TIMOSReaderWriter  #This test fails
-# ctest -R Test_MCML #This test fails
-ctest -R Test_COMSOLMeshReaderWriter 
-# ctest -R Test_TetraEmitterFactory # This test fails
-# ctest -R Test_MCMLKernel_Sample # This test fails
-# ctest -R Test_InternalKernel # This test fails
-ctest -R Test_Mouse
-# ctest -R Test_BlockRandomDistribution # This test fails
-ctest -R Test_RNG
-# ctest -R DVH # This test fails
-# ctest -R DSH # This test fails
-# The following tests should succeed if they run and generate output for comparison, working on them
- ctest -R Test_tclShell
- ctest -R Test_FullMonte_MMC_Comparison
- ctest -R Test_FullMonte_Self
- ctest -R Test_FullMonte_Approx
- ctest -R Test_FullMonte_Composite
-
+call_Tests
 
 
 # Release - GCC 4.9 - VTK 6.3 - Boost 1.60.0
 cd $SRC
-
  cd FullMonteSW/Build/ReleaseGCC
+call_Tests
 
-# ctest -R Test_VTK # does not run
-ctest -R Test_AffineTransform
-ctest -R Test_Units
-ctest -R Test_Layered_Basic
-ctest -R Test_Queries
-# ctest -R Test_TIMOSReaderWriter  #This test fails
-# ctest -R Test_MCML #This test fails
-ctest -R Test_COMSOLMeshReaderWriter 
-# ctest -R Test_TetraEmitterFactory # This test fails
-# ctest -R Test_MCMLKernel_Sample # This test fails
-# ctest -R Test_InternalKernel # This test fails
-ctest -R Test_Mouse
-# ctest -R Test_BlockRandomDistribution # This test fails
-ctest -R Test_RNG
-# ctest -R DVH # This test fails
-# ctest -R DSH # This test fails
-# The following tests should succeed if they run and generate output for comparison, working on them
- ctest -R Test_tclShell
- ctest -R Test_FullMonte_MMC_Comparison
- ctest -R Test_FullMonte_Self
- ctest -R Test_FullMonte_Approx
- ctest -R Test_FullMonte_Composite
 
 fi
 
